a4f49c7e551b9e0dbb0617019b3be60e79c40355
Rafa Brown <shane.rafael@zohomail.com>
2021-08-28 13:23:46 -0600

    Finish styling 2-player version

--- app.js
+++ app.js
@@ -72,11 +72,17 @@ function submitNames () {
     let player1Name = document.getElementById('player1-name').value;
     let player2Name = document.getElementById('player2-name').value;
     if (player1Name === '' || player2Name === '') {
+        document.querySelector('h1').style.fontSize = '1.5rem';
         document.querySelector('h1').innerText = 'Come on, they can be fake names';
-        setTimeout(() => {document.querySelector('h1').innerText = 'Tic Tac Toe';}, 3000)
+        setTimeout(() => {
+            document.querySelector('h1').style.fontSize = '3rem';
+            document.querySelector('h1').innerText = 'Tic Tac Toe';
+        }, 3000)
         return;
     }
     console.log(player1Name, player2Name);
+    player1Name = player1Name.toUpperCase();
+    player2Name = player2Name.toUpperCase();
     inputPlayerNames(1, player1Name);
     inputPlayerNames(2, player2Name);
     whoGoesFirst();
@@ -93,6 +99,7 @@ function newGame () {
     for (let cell of cells) {
         cell.innerText = '';
     }
+    document.querySelector('h1').style.fontSize = '3rem';
     document.querySelector('h1').innerText = 'Tic Tac Toe';
     document.getElementById('board').style.visibility = 'visible';
     gameState.playerO = null;
@@ -145,8 +152,10 @@ function endGame () {
     let winner = gameState.winner;
     document.getElementById('board').style.visibility = 'hidden';
     if (winner !== null) {
+        document.querySelector('h1').style.fontSize = '1.5rem';
         document.querySelector('h1').innerText = `${winner} has won the game!`;
     } else {
+        document.querySelector('h1').style.fontSize = '1.5rem';
         document.querySelector('h1').innerText = "Uh oh - there's a draw!";
     }
 }
